9f9e67a7499946a4e41a9346c241e9979262b57a
Zachary Bys <zachary.bys@gmail.com>
2017-11-25 13:10:55 -0500

    Initial mobile app commit

---
+++ app/src/main/java/quackathon/avalon/HomePage.java
@@ -0,0 +1,13 @@
+package quackathon.avalon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
