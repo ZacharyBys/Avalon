d5813301fc004766cf3ef14705c7eda07ff9778b
Zachary Bys <zachary.bys@gmail.com>
2017-11-26 05:49:24 -0500

    vibrate on notification

--- app/src/main/java/quackathon/avalon/UpdateInfoPage.java
+++ app/src/main/java/quackathon/avalon/UpdateInfoPage.java
@@ -1,12 +1,14 @@
 package quackathon.avalon;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -435,6 +437,8 @@ public class UpdateInfoPage extends AppCompatActivity {
 
                 setRef.push().setValue(notification);
 
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(800);
             }
         });
 
